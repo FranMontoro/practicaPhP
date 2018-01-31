a6571ba06af44460f7493cde808c4c89c37ce2a3
Ubuntu <ubuntu@nginx>
2018-01-31 12:53:15 +0000

    ejercicio Info Servidor .PHP, actualizado

--- codephp/infoserver.php
+++ codephp/infoserver.php
@@ -1,19 +1,20 @@
 <?php
 
 //Nombre Ip del Servidor
-echo $_SERVER["SERVER_ADDR"];
+//Al tener corchete la Variable debemos concatenar el Texto = "texto".$variable[];
+echo "IP: ".$_SERVER["SERVER_ADDR"];
 echo "<br>";
 //Nombre del Host del Servidor
-echo $_SERVER["SERVER_NAME"];
+echo "Nombre Host Server: ".$_SERVER["SERVER_NAME"];
 echo "<br>";
 //El sistema Operativo que usa el Server
-echo $_SERVER["SERVER_SOFTWARE"];
+echo "Sistema Operativo Server: ".$_SERVER["SERVER_SOFTWARE"];
 echo "<br>";
 //Info sobre el agente de usuario que solicita el Script
-echo $_SERVER["HTTP_USER_AGENT"];
+echo "Info Agente: ".$_SERVER["HTTP_USER_AGENT"];
 echo "<br>";
 //La IP desde que esta viendo el usuario
-echo $_SERVER["REMOTE_ADDR"];
-
+echo "IP usuario: ".$_SERVER["REMOTE_ADDR"];
+echo "<br>";
 
 ?>
